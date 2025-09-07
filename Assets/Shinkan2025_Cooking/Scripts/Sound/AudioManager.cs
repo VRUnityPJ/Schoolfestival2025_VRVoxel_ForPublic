642e22a9e5a811296161ef874ad494b175a46b2c
nattohikoki <nattohikoki@gmail.com>
2025-09-07 20:19:19 +0900

    Add: #3 Cookingからの名前入力シーンの移植、エラーあり

---
+++ Assets/Shinkan2025_Cooking/Scripts/Sound/AudioManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using VContainer;
+using UniRx;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip _audioClip;
+    [Inject] private IGameProgressIndicatable _progressIndicatable;
+    [Inject] private IRecipeObservable _recipeObservable;
+
+    public void Start()
+    {
+        _progressIndicatable.OnStartGame
+            .Subscribe(_ => _audioSource.Play()).AddTo(this);
+
+        _recipeObservable.RecipeName
+             .Subscribe(_ => OnPlaySound());
+    }
+
+    public void OnPlaySound()
+    {
+        _audioSource.PlayOneShot(_audioClip);
+    }
+
+}
