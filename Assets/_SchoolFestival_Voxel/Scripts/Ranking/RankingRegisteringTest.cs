45e6695ee69e7ae9e9b2ae57104ea5894d60d291
nattohikoki <nattohikoki@gmail.com>
2025-09-24 22:00:33 +0900

    refactor: #17 RankingRegisteringTest.csの修正

--- Assets/_SchoolFestival_Voxel/Scripts/Ranking/RankingRegisteringTest.cs
+++ Assets/_SchoolFestival_Voxel/Scripts/Ranking/RankingRegisteringTest.cs
@@ -1,30 +1,16 @@
 using Ranking.Scripts;
-using Shinkan2024.Scripts.Game.Point;
 using UnityEngine;
-using R3;
-using Ranking.Demo.Scripts.DemoGame;
 
 public class RankingRegisteringTest : MonoBehaviour
 {
     [SerializeField]
-    private PlayerScoreHolder playerScoreHolder;
-    [SerializeField]
-    private RankingStorage rankingStorage;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private RankingStorage _rankingStorage;
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            // ReactiveProperty<Point> point = new ReactiveProperty<Point>();
-            // point.Value = new Point(122);
-            // pointHolder.SendData(point.Value);
-            rankingStorage.Register();
+            _rankingStorage.Register();
             Debug.Log("Registered");
         }
     }
