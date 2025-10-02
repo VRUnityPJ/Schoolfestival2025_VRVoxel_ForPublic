58342ba9beefabeb9ac52c1b9840c6f2f4adcff3
ARIHI-8 <0ali8hi26@gmail.com>
2025-10-02 16:30:09 +0900

    VRの移動とVoxelの移動を合体、ノートパソコンから一旦デスクトップに移したいのでプッシュだけする

---
+++ Assets/SchoolFestival_Voxel/Scripts/Player/PlayerInputManager.cs
@@ -0,0 +1,58 @@
+using System;
+using R3;
+using UnityEngine;
+using SchoolFestival_Voxel.Scripts.Player.Interfaces;
+namespace SchoolFestival_Voxel.Scripts.Player
+{
+    public class PlayerInputManager : MonoBehaviour,IPlayerInputManager
+    {
+        public Observable<Unit> OnShootLeftWire => _onShootLeftWire;
+        public Observable<Unit> OnShootRightWire => _onShootRightWire;
+        public Observable<Unit> OnReleaseLeftWire => _onReleaseLeftWire;
+        public Observable<Unit> OnReleaseRightWire => _onReleaseRightWire;
+        public Observable<Unit> OnBoostLeft => _onBoostLeft;
+        public Observable<Unit> OnBoostRight => _onBoostRight;
+        public Observable<Unit> OnInputLeftTrigger => _onInputLeftTrigger;
+        public Observable<Unit> OnInputRightTrigger => _onInputRightTrigger;
+        public Observable<Vector2> OnTurn => _onTurn;
+        public Observable<Vector2> OnMove => _onMove;
+        
+        private readonly Subject<Unit> _onShootLeftWire = new();
+        private readonly Subject<Unit> _onShootRightWire = new();
+        private readonly Subject<Unit> _onReleaseLeftWire = new();
+        private readonly Subject<Unit> _onReleaseRightWire = new();
+        private readonly Subject<Unit> _onBoostLeft = new();
+        private readonly Subject<Unit> _onBoostRight = new();
+        private readonly Subject<Unit> _onInputLeftTrigger = new();
+        private readonly Subject<Unit> _onInputRightTrigger = new();
+        private readonly Subject<Vector2> _onTurn = new();
+        private readonly Subject<Vector2> _onMove = new();
+        
+        private MainInput _mainInput;
+
+        private void Awake()
+        {
+            // Enable Input
+            _mainInput = new MainInput();
+            _mainInput.Enable();
+            
+            // Subscribe Input Events
+            /*
+            _mainInput.Player.LeftWireShot.started  += _ => _onShootLeftWire.OnNext(Unit.Default);
+            _mainInput.Player.RightWireShot.started += _ => _onShootRightWire.OnNext(Unit.Default);
+            _mainInput.Player.LeftWireShot.canceled  += _ => _onReleaseLeftWire.OnNext(Unit.Default);
+            _mainInput.Player.RightWireShot.canceled += _ => _onReleaseRightWire.OnNext(Unit.Default);
+            _mainInput.Player.LeftGrappleBoost.started += _ => _onBoostLeft.OnNext(Unit.Default);
+            _mainInput.Player.RightGrappleBoost.started += _ => _onBoostRight.OnNext(Unit.Default);
+            */
+            _mainInput.Player.Turn.started += context => _onTurn.OnNext(context.ReadValue<Vector2>());
+        }
+        
+        // RigidBodyなどの物理演算に関わるものはUpdateではなくFixedUpdateで処理する
+        private void FixedUpdate()
+        {
+            // 移動入力を毎フレーム送信
+            _onMove.OnNext(_mainInput.Player.Move.ReadValue<Vector2>());
+        }
+    }
+}
---
+++ Assets/SchoolFestival_Voxel/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,109 @@
+using NaughtyAttributes;
+using R3;
+using SchoolFestival_Voxel.Scripts.Player.Interfaces;
+using UnityEngine;
+using VContainer;
+using System;
+namespace SchoolFestival_Voxel.Scripts.Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        public float CurrentMoveSpeed => _rigidbody.linearVelocity.magnitude;
+        public float MaxSpeed => _maxSpeed;
+        [Header("Required Components")]
+        [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private Camera _camera;
+        
+        /// <summary>
+        /// 最大速度
+        /// </summary>
+        [Header("Parameters")]
+        [SerializeField] private float _maxSpeed = 5f;
+        
+
+        [Header("Ground Cast Setting")] 
+        [SerializeField] private Vector3 _groundCastBoxSize = new Vector3(0.5f, 0.5f, 0.5f);
+        [SerializeField] private Vector3 _groundCastBoxOffset = Vector3.zero;
+        /// <summary>
+        /// 地面となるレイヤー
+        /// </summary>
+        [SerializeField] private LayerMask _groundLayer;
+
+        private readonly ReactiveProperty<bool> _isGround = new(false);
+        private IPlayerInputManager _playerInputManager;
+        private MainInput _mainInput;
+        private bool _isMovable = true;
+        
+        /// <summary>
+        /// これがVContainer？よくわからない…
+        /// </summary>
+        /// <param name="playerInputManager"></param>
+        [Inject]
+        public void Constructor(IPlayerInputManager playerInputManager)
+        {
+            _playerInputManager = playerInputManager;
+        }
+
+        private void Start()
+        {
+            _playerInputManager.OnMove
+                .Subscribe(input => Move(input))
+                .AddTo(this);
+            _isGround
+                .Subscribe(OnChangeIsGround)
+                .AddTo(this);
+            /*
+            GameJudge.instance?
+                .OnGameOver
+                .Subscribe(_ => _isMovable = false)
+                .AddTo(this);
+                */
+        }
+
+        private void Update()
+        {
+            _isGround.Value = CheckIsGround();
+        }
+
+        /// <summary>
+        /// プレイヤーを移動させるメソッド
+        /// </summary>
+        /// <param name="inputValue">移動方向を計算するための</param>
+        private void Move(Vector2 inputValue)
+        {
+            if (!_isMovable) return;
+            Transform cameraTransform = _camera.transform;
+            
+            // カメラの方向をXZ軸のみ取得
+            Vector3 cameraForward = cameraTransform.forward * inputValue.y;
+            Vector3 cameraRight   = cameraTransform.right   * inputValue.x;
+            Vector3 moveDirection = (cameraForward + cameraRight).normalized;
+            Vector3 targetVelocity = moveDirection * _maxSpeed;
+            
+            // XZ平面のみを考慮する
+            targetVelocity.y = 0f;
+
+            // 滞空時は移動量を調整する
+            if (!_isGround.Value)
+                // targetVelocity *= _airMoveMultiplier;
+            
+            _rigidbody.AddForce(targetVelocity, ForceMode.Force);
+        }
+
+        private void OnChangeIsGround(bool isGround)
+        {
+            // _rigidbody.linearDamping = isGround ? _groundDrag : _airDrag;
+        }
+
+        private bool CheckIsGround()
+        {
+            var origin = transform.position + _groundCastBoxOffset;
+            return Physics.CheckBox(origin, _groundCastBoxSize / 2f, transform.rotation, _groundLayer);
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.DrawWireCube(transform.position + _groundCastBoxOffset, _groundCastBoxSize);
+        }
+    }
+}
